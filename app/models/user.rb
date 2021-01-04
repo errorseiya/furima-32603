6b0835ad240d208433dd1b45779ef777f73b4345
errorseiya <errorseiya@gmail.com>
2021-01-04 21:38:47 +0900

    userモデルでパスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   with_options presence: true do
     validates :nickname 
