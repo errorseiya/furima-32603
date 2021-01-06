ff040e0119fc5b51fccbc91eb3c9d33b10acc0b6
errorseiya <errorseiya@gmail.com>
2021-01-06 19:36:48 +0900

    ActiveStorage「Category」モデル作成、アソシエーション・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+
+  with_options presence: true do
+    validates :name 
+    validates :comment
+    validates :price
+  end
+    
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id, 
+  end
+    
 end
