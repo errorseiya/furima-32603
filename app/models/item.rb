25deee4c8449421c0a1caade90939d9c903d7eb6
errorseiya <errorseiya@gmail.com>
2021-01-07 12:23:27 +0900

    ActiveStorage「Days_to_ship」モデル作成、アソシエーション・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,16 +11,18 @@ class Item < ApplicationRecord
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_charges
-  belongs_to :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_ship
     
   with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
     validates :shipping_charges_id
     validates :prefecture_id
+    validates :days_to_ship_id
   end
     
 end
