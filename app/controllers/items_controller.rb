42ffcc3e1b9297c0c0cb16c4bfe4134a94043906
errorseiya <errorseiya@gmail.com>
2021-01-07 15:54:18 +0900

    商品出品機能のitems_controlle、index,new,createを定義・記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :create]
+
   def index
+  end
+  
+  def new
+    @item = Item.new
+    @category = Category.order("created_at DESC")
+    @condition = Caondition.order("created_at DESC")
+    @shipping_charges = Shipping_charges.order("created_at DESC")
+    @prefecture = Prefecture.order("created_at DESC")
+    @days_to_ship = Days_to_ship.order("created_at DESC")
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item .save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prototype_params  # プライベートメソッド
+    params.require(:item).permit(
+      :image, :name, :comment, :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
