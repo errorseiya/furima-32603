dadfbe5db37aae54eb78113b1843e7039ba63f18
errorseiya <errorseiya@gmail.com>
2021-01-07 12:06:17 +0900

    ActiveStorage「Prefecture」モデル作成、アソシエーション・バリデーション設定と配列微調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,21 +3,24 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-
-
   with_options presence: true do
+    validates :image
     validates :name 
     validates :comment
     validates :price
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charges
+  belongs_to :prefecture
     
-  with_options numericality: { other_than: 1 }  do
+  with_options numericality: { other_than: 0 } do
     validates :category_id
     validates :condition_id
-    validates :shipping_charges
+    validates :shipping_charges_id
+    validates :prefecture_id
   end
     
 end
