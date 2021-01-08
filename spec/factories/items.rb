57a17cf0242d3aa3f311fb175eb6a4409c3038e9
errorseiya <errorseiya@gmail.com>
2021-01-08 16:17:47 +0900

    商品出品機能のバリデーション

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,21 +1,25 @@
 FactoryBot.define do
   factory :item do
-    name              {"ナナシ"}
-    comment                {Faker::Internet.free_email}
-    category              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {"名無しの"}
-    first_name            {"権平"}
-    last_name_kana        {"ナナシノ"}
-    first_name_kana       {"ゴンベイ"}
-    birthday              {'2014-09-23'}
+    name                 {Faker::Creature::Animal.name}
+    comment              {"買ってください"}
+    category_id          {"1"}
+    condition_id         {"1"}
+    shipping_charge_id   {"1"}
+    prefecture_id        {"1"}
+    days_to_ship_id      {"1"}
+    price                {Faker::Number.between(from: 300, to: 9999999)}
+    # from: , toで整数になってる
+    #  {Faker::Number.between(300, 9999999)}⬅︎小数点も出力してしまう
+   
+    
 
+   
+
+    association :user
+    
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
     
   end
 end
-
-image, :name, :comment, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price
-    ).merge(user_id: current_user.id)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "商品出品機能" do
+    context '商品出品がうまくいくとき' do
+      it "image, name, comment, category_id, condition_id, shipping_charge_id, prefecture_id, days_to_ship_id, priceが存在すれば商品出品できる" do
+        expect(@item).to be_valid
+      end
+      it "priceが300〜9999999であれば商品出品できる" do
+        @item.price = "300"
+        expect(@item).to be_valid
+      end
+      it "priceが半角数字であれば商品出品できる" do
+        @item.price = "500"
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+      it "imageが空だと商品出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it "nameが空では商品出品できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it "commentが空では商品出品できない" do
+        @item.comment = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Comment can't be blank"
+      end
+      it "category_idのidが0では商品出品できない" do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 0"
+      end
+      it "condition_idのidが0では商品出品できない" do
+        @item.condition_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition must be other than 0"
+      end
+      it "shipping_charge_idのidが0では商品出品できない" do
+        @item.shipping_charge_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping charge must be other than 0"
+      end
+      it "prefecture_idのidが0では商品出品できない" do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture must be other than 0"
+      end
+      it "days_to_ship_idのidが0では商品出品できない" do
+        @item.days_to_ship_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Days to ship must be other than 0"
+      end
+      it "priceが空では商品出品できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+
+      it "priceが、299以下では商品出品できない" do
+        @item.price = "100"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be greater than 299"
+      end
+   
+      it "priceが、10,000,000以上では商品出品できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be less than 10000000"
+      end
+
+      it "priceが半角数字のみ保存可能では商品出品できない" do
+        @item.price = "５００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+    end
+  end
 end
