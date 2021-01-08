9864d148c70453f6139575d909bb05365500464f
errorseiya <errorseiya@gmail.com>
2021-01-08 19:54:00 +0900

    販売手数料は10%としています。またMath.ceilメソッドで1円単位に切り上げ

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = Math.ceil(inputValue * 0.1);
     // console.log(addTaxDom);
     // 引数として与えられている()内の計算式の結果の小数点以下を切り捨ててくれます
 
