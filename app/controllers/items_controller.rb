783cfd971b3d7dc92a2b913bd6112767df22273e
errorseiya <errorseiya@gmail.com>
2021-01-11 16:27:07 +0900

    /items_controller.rbをリファクタリング修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :edit_Identification, only: [:edit]
 
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def edit_Identification
-    if user_signed_in? && current_user.id != @item.user_id
+    if current_user.id != @item.user_id
       redirect_to root_path  
     end
   end
