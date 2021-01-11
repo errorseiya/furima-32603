b4829bb4b4e90c0617eb7c36146da49edb57741a
errorseiya <errorseiya@gmail.com>
2021-01-11 15:27:23 +0900

    編集機能のitems_controller.rb、edit、update等を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :edit_Identification, only: [:edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,12 +21,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  
 
   private
 
@@ -32,5 +42,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :image, :name, :comment, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def edit_Identification
+    if user_signed_in? && current_user.id != @item.user_id
+      redirect_to root_path  
+    end
   end
 end
