91cc9a2ca80cdaf3efa8d2a8a95fe38baf6d8084
errorseiya <errorseiya@gmail.com>
2021-01-15 12:09:45 +0900

    購入機能モデルformオブジェクト完成１

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,17 +1,22 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :buyer_id
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :buyer_id, :token
 
   
   # ここにバリデーションの処理を書く
   with_options presence: true do
-    # 「住所」の郵便番号に関するバリデーション
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-) or  please enter in half-width numbers" }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :municipality
     validates :address
-    validates :phone_number, numericality:  {only_integer: true, message: "is please enter in half-width numbers"}
+    validates :phone_number
+    validates :token
+    validates :user_id
+    validates :item_id
   end
+
+  # 固定電話(10桁),携帯電話(11桁)の正規表現
+  validates :phone_number, format: { with: /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "is invalid. Include hyphen(-) or  please enter in half-width numbers" }
 
   def save
     # 各テーブルにデータを保存する処理を書く
