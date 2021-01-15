793fa3290ecef96dbf92f021963a3d5600c1cb4a
errorseiya <errorseiya@gmail.com>
2021-01-15 12:11:11 +0900

    購入機能テストコードfactories記述

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :buyer do
-    
+  factory :buyer_address do
+    postal_code     { '123-4567' }
+    prefecture_id   { Faker::Number.between(from: 1, to: 47 )}
+    municipality    { '世田谷区' }
+    address         { '上馬1−2−３' }
+    building_name   { '一号室' }
+    phone_number    { '09012345678' }
+    token            {"tok_abcdefghijk00000000000000000" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              {"ナナシ"}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    # ⬇⬇⬇︎パスワードは英数混合なので始めに混合のものを記述してあげる
+    password              {'0a' + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     last_name             {"名無しの"}
     first_name            {"権平"}
@@ -11,5 +12,4 @@ FactoryBot.define do
     birthday              {'2014-09-23'}
   end
 end
-
 
